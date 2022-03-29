7a11c79a9b1e3260d92cffd7da1ebfdf8eca7b25
stolpe22 <57258862+stolpe22@users.noreply.github.com>
2022-03-28 23:55:27 -0400

    Criação do PrintServiceString e GitIgnore Incluído

--- List_Set_Generics/Program.cs
+++ List_Set_Generics/Program.cs
@@ -1,14 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using List_Set_Generics;
 
-PrintService printService = new PrintService();
+PrintServiceString printService = new PrintServiceString();
 
 Console.Write("How many values? ");
 int n = int.Parse(Console.ReadLine());
 
 for (int i = 0; i < n; i++)
 {
-    int x = int.Parse(Console.ReadLine());
+    string x = Console.ReadLine();
     printService.AddValue(x);
 }
 
