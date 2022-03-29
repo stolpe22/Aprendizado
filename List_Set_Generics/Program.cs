5e906b85322c5cca1633e398ae2f33aaca993f0a
stolpe22 <57258862+stolpe22@users.noreply.github.com>
2022-03-29 00:48:21 -0400

    Entendeno TypeSafety
    O tipo object aceita qualquer tipo e o compilador não identifica o erro durante a codificação, o Intelisense não aponta nada.

--- List_Set_Generics/Program.cs
+++ List_Set_Generics/Program.cs
@@ -8,7 +8,7 @@ int n = int.Parse(Console.ReadLine());
 
 for (int i = 0; i < n; i++)
 {
-    int x = int.Parse(Console.ReadLine());
+    string x = Console.ReadLine();
     printService.AddValue(x);
 }
 
