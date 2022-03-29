b81d72baaa1fd761e60c0598496b54668b7af22e
stolpe22 <57258862+stolpe22@users.noreply.github.com>
2022-03-28 23:46:13 -0400

    Commit inicial de Generics, Set, Dictionary

---
+++ List_Set_Generics/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using List_Set_Generics;
+
+PrintService printService = new PrintService();
+
+Console.Write("How many values? ");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    int x = int.Parse(Console.ReadLine());
+    printService.AddValue(x);
+}
+
+printService.Print();
+Console.WriteLine("First: " + printService.First());
