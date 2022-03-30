2b92f9b4f187c333975167cedaf01d8f4f382f0f
stolpe22 <57258862+stolpe22@users.noreply.github.com>
2022-03-30 00:28:16 -0400

    Equals, ==, GetHashCode CU-22bqe49

--- GetHashCode_Equals/Program.cs
+++ GetHashCode_Equals/Program.cs
@@ -4,5 +4,9 @@ using GetHashCode_Equals.Entities;
 Client client = new Client { Name = "Maria", Email = "maria@gmail.com" };
 Client client2 = new Client { Name = "Alex", Email = "maria@gmail.com" };
 
-//Os emails são iguais, logo a comparação entre os objetos retornará true;
-Console.WriteLine(client.Equals(client2));
+
+Console.WriteLine(client.Equals(client2));// Os emails são iguais, logo a comparação Equals() entre os objetos retornará true;
+Console.WriteLine(client == client2);// O operador == compara referência do ponteiro de memória do objeto, sendo duas instâncias diferentes, o resultado é false
+//GetHashCode obtendo seu valor a partir da propriedade Email, conforme override
+Console.WriteLine(client.GetHashCode());
+Console.WriteLine(client2.GetHashCode());
