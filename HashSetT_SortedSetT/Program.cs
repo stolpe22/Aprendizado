83b2e7adc4f4cf3c032c6f17af08424181492426
stolpe22 <57258862+stolpe22@users.noreply.github.com>
2022-03-31 00:33:46 -0400

    IntersectWith e ExceptWith

--- HashSetT_SortedSetT/Program.cs
+++ HashSetT_SortedSetT/Program.cs
@@ -7,8 +7,19 @@ SortedSet<int> b = new SortedSet<int>() { 5, 6, 7, 8, 9, 10 };
 SortedSet<int> c = new SortedSet<int>(a);
 //Aqui o conjunto c une-se ao cojunto b, sem repetir os elementos devido à restrição. Logo, o resultado é 0 2 4 5 6 7 8 9 10
 c.UnionWith(b);
-
 PrintCollection(c);
+
+//intersection
+SortedSet<int> d = new SortedSet<int>(a);
+//Retorna apenas os valores em comum nas duas coleções
+d.IntersectWith(b);
+PrintCollection(d);
+
+//difference
+SortedSet<int> e = new SortedSet<int>(a);
+//Retorna apenas os valores divergentes entre as duas coleções
+e.ExceptWith(b);
+PrintCollection(e);
 
 static void PrintCollection<T>(IEnumerable<T> collection)
 {
