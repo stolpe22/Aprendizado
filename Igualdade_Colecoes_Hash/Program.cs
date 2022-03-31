76c96ced41b740a164bdeefc9eda6d9bd11f4025
stolpe22 <57258862+stolpe22@users.noreply.github.com>
2022-03-31 01:09:32 -0400

    Comparação tipo valor Hash
    Tipo valor sendo comparado sem implementação do GetHashCode ou Equals. Funciona porque os valores são comparados, e não os ponteiros na memória

--- Igualdade_Colecoes_Hash/Program.cs
+++ Igualdade_Colecoes_Hash/Program.cs
@@ -12,6 +12,11 @@ HashSet<Point> points = new HashSet<Point>();
 points.Add(new Point(3, 4));
 points.Add(new Point(5, 10));
 
-Product product2 = new Product("Notebook", 1200.00);
+Product product = new Product("Notebook", 1200.00);
 //GetHashCode e Equals foram implementados na classe Product, a comparação do Contains, agora, retorna FALSE;
-Console.WriteLine(products.Contains(product2));
+Console.WriteLine(products.Contains(product));
+
+Point point = new Point(5, 10);
+//Neste caso, mesmo não tendo implementado GetHashCode e Equals, Contains retorna TRUE.
+//Quando é tipo valor (e.g. struct) os valores são comparados, não os ponteiros na memória.
+Console.WriteLine(points.Contains(point));
