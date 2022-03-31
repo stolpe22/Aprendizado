755b7d1565360274a6354ce29685543f556ccda4
stolpe22 <57258862+stolpe22@users.noreply.github.com>
2022-03-31 00:43:59 -0400

    Exemplo prático
    Se GetHashCode e Equals estiverem implementados. Este aqui:

--- Igualdade_Colecoes_Hash/Program.cs
+++ Igualdade_Colecoes_Hash/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Fundamentos e Conceitos de comparação
+
+HashSet<string> set = new HashSet<string>();
+
+set.Add("Maria");
+set.Add("Alex");
+
+//Se GetHashCode e Equals estiverem implementados:
+//Primeiro GetHashCode. Se der igual, ele usa Equals para confirmar
+//Se GetHashCode e Equals NÃO estiverem implementados:
+//Tipos referência: compara as referências dos objetos
+//Tipos valor: compara os valores dos atributos
+Console.WriteLine(set.Contains("Maria"));
