f011e12a9bb1e0814728828d664456d3fa75ecab
stolpe22 <57258862+stolpe22@users.noreply.github.com>
2022-03-31 23:57:54 -0400

    Desenvolvimento
    Exercicio desenvolvido

--- Exercicio_Cojuntos/Program.cs
+++ Exercicio_Cojuntos/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Enter file full path: ");
+string path = Console.ReadLine();
+try
+{
+    using (StreamReader sr = File.OpenText(path))
+    {
+        while (!sr.EndOfStream)
+        {
+            string line = sr.ReadLine();
+            Console.WriteLine(line);
+        }
+    }
+}
+catch (IOException e)
+{
+    Console.WriteLine(e.Message);
+}
