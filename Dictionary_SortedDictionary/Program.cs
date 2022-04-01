507421744bd907449bb54d70bca8f213d08825fb
stolpe22 <57258862+stolpe22@users.noreply.github.com>
2022-04-01 00:32:12 -0400

    Alguns métodos e um erro
    Utilizando metodo remove e criando o erro KeyNotFoundException

--- Dictionary_SortedDictionary/Program.cs
+++ Dictionary_SortedDictionary/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Dictionary<string, string> cookies = new Dictionary<string, string>();
+
+cookies["user"] = "maria";
+cookies["email"] = "maria@gmail.com";
+cookies["phone"] = "9988776655";
+cookies["phone"] = "5544332211";//Este valor sobrescreverá o valor anterior da chave "phone", por não permitir repetições de chave
+
+Console.WriteLine(cookies["phone"]);
+Console.WriteLine(cookies["email"]);
+
+cookies.Remove("email");
+
+Console.WriteLine(cookies["email"]);//Neste caso retorna erro KeyNotFoundException
