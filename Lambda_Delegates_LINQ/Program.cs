045acc83659cec2b78551dff3843fdd3d8757b97
stolpe22 <57258862+stolpe22@users.noreply.github.com>
2022-04-03 02:22:36 -0400

    Multicast Delegates
    • Delegates que guardam a referência para mais de um método
    • Para adicionar uma referência, pode-se usar o operador +=
    • A chamada Invoke (ou sintaxe reduzida) executa todos os métodos na
    ordem em que foram adicionados
    • Seu uso faz sentido para métodos void

--- Lambda_Delegates_LINQ/Program.cs
+++ Lambda_Delegates_LINQ/Program.cs
@@ -3,6 +3,7 @@
 namespace Lambda_Delegates
 {
     delegate double BinaryNumericOperation(double n1, double n2);
+    delegate void BinaryNumericOperationMulticastDelegates(double n1, double n2);
     class Program
     {
         static void Main(string[] args)
@@ -10,8 +11,12 @@ namespace Lambda_Delegates
             double a = 10;
             double b = 12;
 
-            BinaryNumericOperation op = CalculationService.Square; //Dá erro porque a assinatura do método Square não condiz com o que foi definido no delegate 
+            BinaryNumericOperation op = CalculationService.Sum;
 
+            BinaryNumericOperationMulticastDelegates op2 = CalculationServiceMulticastDelegates.ShowSum;
+            op2 += CalculationServiceMulticastDelegates.ShowMax;
+
+            op2.Invoke(a, b);
             double result = op(a, b);
             Console.WriteLine(result);
 
