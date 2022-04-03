c612059f1bf12892b1958b3a25916f59d1dd2989
stolpe22 <57258862+stolpe22@users.noreply.github.com>
2022-04-03 02:35:56 -0400

    Predicate - RemoveAll(Predicate<in T> match)
    Sendo T um objeto
    
    Predicate é um delegate, é uma referência para uma função .
    No caso do predicate o delegate define que tem que ser uma função que recebe o objeto e devolve o booleano. Então qualquer função que voce referenciar no RemoveAll(), desde que a função atenda a assinatura bool Funcao(objeto T), ela funcionará.
    Pode-se, então colocar uma expressão lambda como parametro para o RemoveAll() ou utilizar um método que combine com a assinatura.

---
+++ Predicate/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using Predicate.Entities;
+
+List<Product> list = new List<Product>();
+list.Add(new Product("Tv", 900.00));
+list.Add(new Product("Mouse", 50.00));
+list.Add(new Product("Tablet", 350.50));
+list.Add(new Product("HD Case", 80.90));
+
+list.RemoveAll(ProductTest);
+
+foreach(Product p in list)
+{
+    Console.WriteLine(p);
+}
+
+bool ProductTest(Product product)
+{
+    return product.Price >= 100.0;
+}
