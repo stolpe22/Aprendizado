4ba9643996d094db85fa942df48b5ed258dda674
stolpe22 <57258862+stolpe22@users.noreply.github.com>
2022-04-03 02:08:32 -0400

    Usando Delegate
    É uma referência (com type safety) para um ou mais métodos
    -É um tipo referência
    
    Usos comuns
    -Comunicação entre objetos de forma flexífel e extensível (eventos / callbacks)
    -Parametrização de operações por métodos (programação funcional)

--- Lambda_Delegates_LINQ/Program.cs
+++ Lambda_Delegates_LINQ/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Lambda_Delegates_LINQ.Services;
+
+namespace Lambda_Delegates
+{
+    delegate double BinaryNumericOperation(double n1, double n2);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationService.Sum;
+
+            double result = op(a, b);
+            Console.WriteLine(result);
+
+        }
+    }
+
+}
