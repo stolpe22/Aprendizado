dbecaa2d2ceb15b151affb0569a6dc6e86e1cd7b
stolpe22 <57258862+stolpe22@users.noreply.github.com>
2022-04-03 23:51:31 -0400

    Func Exemplo 3

--- Func/Program.cs
+++ Func/Program.cs
@@ -10,9 +10,11 @@ class Program
         list.Add(new Product("Tablet", 350.50));
         list.Add(new Product("HD Case", 80.90));
 
-        Func<Product, string> func = NameUpper; // func recebe Product e retorna string
+        Func<Product, string> func = p => p.Name.ToUpper(); // func recebe Product e retorna string 
+        //Se colocar chaves {}, como no outro exemplo, teria que colocar a palavra return. Porque quando coloca chaves o que a expressão lambda expera é um comando.
+        //Mas como a lambda corresponde à uma função que retorna algo, pode-se apagar o return e as chaves
 
-        List<string> result = list.Select(func).ToList();
+        List<string> result = list.Select(p => p.Name.ToUpper()).ToList();
 
         foreach(string s in result)
         {
