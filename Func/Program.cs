102b53d10e80f6aad9e1b627e64a89ae8a67bbc6
stolpe22 <57258862+stolpe22@users.noreply.github.com>
2022-04-03 23:44:52 -0400

    Func Exemplo 1

--- Func/Program.cs
+++ Func/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Func.Entities;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Product> list = new List<Product>();
+        list.Add(new Product("Tv", 900.00));
+        list.Add(new Product("Mouse", 50.00));
+        list.Add(new Product("Tablet", 350.50));
+        list.Add(new Product("HD Case", 80.90));
+
+        List<string> result = list.Select(NameUpper).ToList();
+
+        foreach(string s in result)
+        {
+            Console.WriteLine(s);
+        }
+    }
+
+    static string NameUpper(Product p)
+    {
+        return p.Name.ToUpper();
+    }
+}
+
