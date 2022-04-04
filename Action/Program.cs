bb170e6eb6e74b466340b001c5aec7d47f6aa667
stolpe22 <57258862+stolpe22@users.noreply.github.com>
2022-04-03 23:34:43 -0400

    Action direto ForEach
    Pode-se colocar a ação diretamente no corpo do ForEach da Lista

--- Action/Program.cs
+++ Action/Program.cs
@@ -7,9 +7,7 @@ list.Add(new Product("Mouse", 50.00));
 list.Add(new Product("Tablet", 350.50));
 list.Add(new Product("HD Case", 80.90));
 
-Action<Product> action = p => { p.Price += p.Price * 0.1; }; //recebe o argumento 'p' e chama uma ação. Para a ação abre e fecha chaves para indicar que a fução tem um corpo mas não vai retornar nada. 
-
-list.ForEach(action);
+list.ForEach(p => { p.Price += p.Price * 0.1; }); //Colocando a ação direto no ForEach
 
 list.ForEach(p => Console.WriteLine(p));
 
