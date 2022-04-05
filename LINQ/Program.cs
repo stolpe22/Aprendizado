118efd53c6b59cccdca69ffe2639ad111ee664e8
stolpe22 <57258862+stolpe22@users.noreply.github.com>
2022-04-04 23:42:00 -0400

    LINQ introdução

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿int[] numbers = new int[] { 2, 3, 4, 5 };
+
+//LINQ é composto por vários extensions methods atrelados às coleções
+var result = numbers
+    .Where(x => x % 2 == 0)
+    .Select(x => x * 10);
+
+foreach (var x in result)
+{
+    Console.WriteLine(x);
+}
