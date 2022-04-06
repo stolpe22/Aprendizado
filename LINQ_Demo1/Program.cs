1361391abb0bd6330772ee9eed25adfc619e2dd5
stolpe22 <57258862+stolpe22@users.noreply.github.com>
2022-04-05 23:52:19 -0400

    Consulta com Where

--- LINQ_Demo1/Program.cs
+++ LINQ_Demo1/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LINQ_Demo1.Entities;
+
+class Program
+{
+    static void Print<T>(string message, IEnumerable<T> collection)
+    {
+        Console.WriteLine(message);
+        foreach (T obj in collection)
+        {
+            Console.WriteLine(obj);
+        }
+        Console.WriteLine();
+    }
+    static void Main(string[] args)
+    {
+        Category c1 = new Category() { Id = 1, Name = "Tools", Tier = 2 };
+        Category c2 = new Category() { Id = 2, Name = "Computers", Tier = 1 };
+        Category c3 = new Category() { Id = 3, Name = "Electronics", Tier = 1 };
+
+        List<Product> products = new List<Product>()
+        {
+            new Product() { Id = 1, Name = "Computer", Price = 1100.0, Category = c2 },
+            new Product() { Id = 2, Name = "Hammer", Price = 90.0, Category = c1 },
+            new Product() { Id = 3, Name = "TV", Price = 1700.0, Category = c3 },
+            new Product() { Id = 4, Name = "Notebook", Price = 1300.0, Category = c2 },
+            new Product() { Id = 5, Name = "Saw", Price = 80.0, Category = c1 },
+            new Product() { Id = 6, Name = "Tablet", Price = 700.0, Category = c2 },
+            new Product() { Id = 7, Name = "Camera", Price = 700.0, Category = c3 },
+            new Product() { Id = 8, Name = "Printer", Price = 350.0, Category = c3 },
+            new Product() { Id = 9, Name = "MacBook", Price = 1800.0, Category = c2 },
+            new Product() { Id = 10, Name = "Sound Bar", Price = 700.0, Category = c3 },
+            new Product() { Id = 11, Name = "Level", Price = 70.0, Category = c1 }
+        };
+
+        var r1 = products.Where(p => p.Category.Tier == 1 && p.Price < 900.00);
+        Print("TIER 1 AND PRICE < 900:", r1);
+
+    }
+}
+
