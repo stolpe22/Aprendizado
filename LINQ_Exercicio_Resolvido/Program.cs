4944d4d74d8cee664ed005ff2310f344ea045abb
stolpe22 <57258862+stolpe22@users.noreply.github.com>
2022-04-08 01:05:34 -0400

    LINQ exercício resolvido

--- LINQ_Exercicio_Resolvido/Program.cs
+++ LINQ_Exercicio_Resolvido/Program.cs
@@ -17,6 +17,12 @@ using (StreamReader sr = File.OpenText(path))
     }
 
     var avg = products.Select(p => p.Price).DefaultIfEmpty(0.0).Average();
-    
+    Console.WriteLine("Average price = " + avg.ToString("F2", CultureInfo.InvariantCulture));
+
+    var names = products.Where(p => p.Price < avg).OrderByDescending(p => p.Name).Select(p => p.Name);
+    foreach(string name in names)
+    {
+        Console.WriteLine(name);
+    }
 
 }
